13e5df85db6bea849b44917c852f4e80e1a3051e
Liana Baryshova <barishovaliana@gmail.com>
2021-09-12 22:10:18 +0200

    Add components

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { Component } from 'react';
+import { ToastContainer } from 'react-toastify';
+import PokemonForm from './components/PokemonForm';
+import PokemonInfo from './components/PokemonInfo';
+
+export default class App extends Component {
+  state = {
+    pokemonName: '',
+  };
+
+  handleFormSubmit = pokemonName => {
+    this.setState({ pokemonName });
+  };
+
+  render() {
+    return (
+      <div style={{ maxWidth: 1170, margin: '0 auto', padding: 20 }}>
+        <PokemonForm onSubmit={this.handleFormSubmit} />
+        <PokemonInfo pokemonName={this.state.pokemonName} />
+        <ToastContainer autoClose={3000} />
+      </div>
+    );
+  }
+}
+
+// componentDidMount() {
+//   this.setState({ loading: true });
+//   setTimeout(() => {
+//     fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+//       .then(res => res.json())
+//       .then(pokemon => this.setState({ pokemon }))
+//       .finally(() => this.setState({ loading: false }));
+//   }, 1000);
+// }
